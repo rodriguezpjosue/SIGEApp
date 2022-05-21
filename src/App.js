c591461fa7bfc0254d6b6de5ca8e2bb641206e48
Josue Rodriguez <josue.rodriguez@ASIGBIEM.ORG>
2022-05-21 10:18:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import Login from './components/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/modern.min.css';
+import './css/custom.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,32 @@
+
+import { Row, Col, Form } from 'react-bootstrap';
+export default function Login(props){
+    return (
+        <main className="page-content">
+            <div className="page-inner">
+                <div id="main-wrapper">
+                    <Row>
+                        <Col md={3} className="center"></Col>
+                        <div className="login-box">
+                            <a href="index.html" className="logo-name text-lg text-center">Modern</a>
+                            <p className="text-center m-t-md">Please login into your account.</p>
+                            <Form className="m-t-md" action="index.html">
+                                <Form.Group>
+                                    <Form.Control type="email" placeholder="Email" required />
+                                </Form.Group>
+                                <div className="form-group">
+                                    <input type="password" className="form-control" placeholder="Password" required/>
+                                </div>
+                                <button type="submit" className="btn btn-success btn-block">Login</button>
+                                <a href="forgot.html" className="display-block text-center m-t-md text-sm">Forgot Password?</a>
+                                <p className="text-center m-t-xs text-sm">Do not have an account?</p>
+                                <a href="register.html" className="btn btn-default btn-block m-t-md">Create an account</a>
+                            </Form>
+                            <p className="text-center m-t-xs text-sm">2015 &copy; Modern by Steelcoders.</p>
+                        </div>
+                    </Row>
+                </div>
+            </div>
+        </main>
+    );
+}
