1145fa95a4233a701c1d1dc67589c3ff604486f3
Josue Rodriguez <rodriguezpjosue@gmail.com>
2022-05-22 15:33:48 -0500

    login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import Login from './components/Login';
 import Informes from './components/Informes';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,10 +7,24 @@ import './css/modern.min.css';
 import './css/custom.css';
 function App() {
   const [logged, setLogin] = useState(false);
-  const login = () => {
-    setLogin(true);
-    alert('logged in!');
+
+  const login = async (event) => {
+    event.preventDefault();
+    const location = 'https://sige.emmanuel.pe/rest/login';
+    const settings = {
+      db: 'sige', 
+      login: 'soporte@ibemmanuel.org', 
+      password: 'J3sus1smystr3ngth'
+    }
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+    const axiosResponse = await axios.post(`${location}`, settings, {headers: headers});
+    console.log(axiosResponse.data.result);
+    return axiosResponse;
   }
+
   return (
     <div className="App">
       {logged 
